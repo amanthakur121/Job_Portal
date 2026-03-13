a3d7b64bfd76416901cb7b5833f623dc683d4dfe
Aman <amanth688@gmail.com>
2026-03-13 13:09:55 +0530

    fix render port

--- backend/server.js
+++ backend/server.js
@@ -13,9 +13,13 @@ app.use(express.json())
 app.use("/api/jobs", jobRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
-.then(()=>console.log("MongoDB connected"))
-.catch(err=>console.log(err))
+.then(()=>{
+    console.log("MongoDB connected")
 
-app.listen(5000,()=>{
-console.log("Server running on port 5000")
+    const PORT = process.env.PORT || 5000
+
+    app.listen(PORT, ()=>{
+        console.log(`Server running on port ${PORT}`)
+    })
 })
+.catch(err=>console.log(err))
